f74fc30087dfa59c059ac07d9044362dcf7a3afa
Brahim Remaigui <119951262+BrahimRemaigui@users.noreply.github.com>
2023-12-06 14:04:34 +0100

    Update Skils.js

--- src/Skils.js
+++ src/Skils.js
@@ -1,53 +1,67 @@
-import React, { useState } from 'react'
-import { useLoaderData, } from "react-router-dom"
-import myImage from './skills.png';
-import FormSk from './FormSk';
-
-
-export default function Skils() {
-    const rews = useLoaderData();
-    const [isClicked, setisClicked] = useState(false);
-    const spanStyle = {
-        width: '80%'
-    };
-    const handeAddSki = () => {
-        setisClicked(!isClicked)
-    }
-    return (
-        <>
-            <div className="our-skills" id="our-skills">
-                <h2 className="main-title">Our Skills</h2>
-                <div className="container">
-                    <img src={myImage} alt="" />
-                    <div className="skills">
-                        {rews.map(rew => (
-
-                            <div className="skill">
-
-                                <h3>{rew.skilsName} <span>{rew.degre}</span></h3>
-                                <div className="the-progress">
-                                    <div className="the-progress">
-                                        <span style={{ width: rew.degre }}></span>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-
-                </div>
+import './Home.css'
+import './Skills.css'
+import img from '../imgs/skills.png'
+export default function Skills(){
+  const degre="80%"
+    return(
+      <div className="skills-container">
+        <div className="space-witi">
+                <h1><span style={{color:'blueviolet'}}>Our </span> Skills </h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius adipisci pariatur aliquid exercitationem debitis, necessitatibus recusandae eveniet dolorum sit quasi corrupti. Tenetur ab, rerum atque eius minus non inventore nesciunt.</p>
+        </div>
+        <div className="con-skill">
+        <div className="our-skills" id="our-skills">
+        <h2 className="main-title"></h2>
+        <div className="container">
+          <img decoding="async" src={img} alt="" />
+          <div className="skills">
+            <div className="skill">
+              <h3>HTML <span>80%</span></h3>
+              <div className="the-progress">
+              <span style={{width: "80%"}}></span>  
+              </div>
             </div>
-            <h2 className="main-title" style={{ cursor: 'pointer' }} onClick={handeAddSki}>Add new skills</h2>
-            {isClicked && <div className='frameAdd'>
-               <FormSk isClicked={isClicked} setisClicked={setisClicked}/>
-            </div>}
-        </>
+            <div className="skill">
+              <h3>CSS <span>85%</span></h3>
+              <div className="the-progress">
+              <span style={{width: "85%"}}></span> 
+              </div>
+            </div>
+            <div className="skill">
+              <h3>JavaScript <span>70%</span></h3>
+              <div className="the-progress">
+              <span style={{width: "70%"}}></span>
+              </div>
+            </div>
+            <div className="skill">
+              <h3>Python <span>80%</span></h3>
+              <div className="the-progress">
+              <span style={{width: "80%"}}></span>
+              </div>
+            </div>
+            <div className="skill">
+              <h3>React.js <span>50%</span></h3>
+              <div className="the-progress">
+              <span style={{width: "50%"}}></span>  
+              </div>
+            </div>
+            <div className="skill">
+              <h3>Vue.js <span>40%</span></h3>
+              <div className="the-progress">
+              <span style={{width: "40%"}}></span>  
+              </div>
+            </div>
+            <div className="skill">
+              <h3>PHP <span>40%</span></h3>
+              <div className="the-progress">
+              <span style={{width: "40%"}}></span>  
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
     )
-}
 
-export const skilsLoader = async () => {
-    const rew = await fetch('http://localhost:4000/skils')
-    if (!rew.ok) {
-        throw Error('Could not found the error !')
-    }
-    return rew
 }
